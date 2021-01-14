49197cb94de0433122a30dc003d75f61c6d75dcd
Ajay Dwivedi <61159945+ajaydwi7@users.noreply.github.com>
2021-01-14 14:20:21 +0530

    Add files via upload

---
+++ people_db.sql
@@ -0,0 +1,7 @@
+insert into people(people_name, people_Email, people_ph, people_gender) values
+('aman', 'aman@gmail.com', '7894561230', 'male'),
+('rahul', 'rahul@gmail.com', '7894561210', 'male'),
+('vivek', 'vivek@gmail.com', '7894561231', 'male'),
+('karan', 'karan@gmail.com', '7894561232', 'male'),
+('ritesh', 'ritesh@gmail.com', '7894561233', 'male'),
+('prachi', 'prachi@gmail.com', '7894561234', 'female')
